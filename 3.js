2b3924b3886783c9df6aeb1e82be6e2187a87e4c
John Weis <weis.john@gmail.com>
2012-11-12 15:25:10 -0500

    adding express example

---
+++ 3.js
@@ -0,0 +1,12 @@
+/*
+	Concept: Hello World - Express
+*/
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(3000);
