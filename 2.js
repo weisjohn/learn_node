80e53d21e5c741a9e3eec38357fa0525bc799a38
John Weis <weis.john@gmail.com>
2012-11-12 11:40:32 -0500

    hello worlds

---
+++ 2.js
@@ -0,0 +1,12 @@
+/*
+	Concept: Hello World - TCP
+*/
+
+var net = require('net');
+
+var server = net.createServer(function (socket) {
+  socket.write('Echo server\r\n');
+  socket.pipe(socket);
+});
+
+server.listen(1337, '127.0.0.1');
