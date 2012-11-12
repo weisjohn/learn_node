fe06110dd16f281f275b83ac37749eb0bcf7af3d
John Weis <weis.john@gmail.com>
2012-11-12 17:37:03 -0500

    splitting out some examples

--- 5.js
+++ 5.js
@@ -3,21 +3,15 @@
 */
 
 var express = require('express'),
-  request = require('request'),
-  debug_express = require('debug')('express'),
-  debug_request = require('debug')('request'),
-  app = express();
+    request = require('request'),
+    app = express();
 
 app.use(function(req, res, next) {
-  debug_express('proxy: ' + req.url);
-  debug_request('request');
-  request.get("http://johnweis.com" + req.url, 
-  function(err, resp) {
-    debug_request('response');
-    res.set(resp.headers);
-    res.send(resp.body);
-    debug_express('done');
-  });
+    request.get("http://johnweis.com" + req.url, 
+        function(err, resp) {
+        res.set(resp.headers);
+        res.send(resp.body);
+    });
 });
 
 app.listen(3000);
---
+++ 6.js
@@ -0,0 +1,23 @@
+/*
+  Concept: Simple HTTP Proxy
+*/
+
+var express = require('express'),
+  request = require('request'),
+  debug_express = require('debug')('express'),
+  debug_request = require('debug')('request'),
+  app = express();
+
+app.use(function(req, res, next) {
+  debug_express('proxy: ' + req.url);
+  debug_request('request');
+  request.get("http://johnweis.com" + req.url, 
+  function(err, resp) {
+    debug_request('response');
+    res.set(resp.headers);
+    res.send(resp.body);
+    debug_express('done');
+  });
+});
+
+app.listen(3000);
