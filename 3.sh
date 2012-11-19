5a1993a6892235d87add12e09dc555849c10afd4
John Weis <weis.john@gmail.com>
2012-11-19 10:58:57 -0500

    write a secure key

---
+++ 3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+path=`which openssl`
+
+if [ "$path" ]; then
+	openssl genrsa -out privatekey.pem 1024 
+	openssl req -new -key privatekey.pem -out certrequest.csr 
+	openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
+else
+	echo "install openssl first (brew install openssl || sudo apt-get install openssl)";
+fi
