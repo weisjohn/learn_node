ae53e1384baf6c82cd5dbe74681da3bfa2cafa3b
John Weis <weis.john@gmail.com>
2012-11-28 15:25:22 -0500

    socket.io example ball thingie

---
+++ 9.js
@@ -0,0 +1,29 @@
+/*
+  Concept: Socket.IO server
+*/
+
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs');
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/10.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading 10.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+var position = { left : 0, top : 0 };
+io.sockets.on('connection', function (socket) {
+  socket.emit('move', position);
+  socket.on('move', function (data) {
+    socket.broadcast.emit("move", position = data);
+  });
+});
