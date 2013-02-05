17f7b12015d4ae16d5b0054244cece6956ea120a
John Weis <weis.john@gmail.com>
2013-02-04 20:51:54 -0500

    adding randal

---
+++ 14.js
@@ -0,0 +1,10 @@
+// Randal helps pick the order for the game
+module.exports = function randal() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    var rand, shuffle = [];
+    while (args.length) { 
+        rand = Math.floor(Math.random() * args.length);
+        shuffle.push(args.splice(rand,1)[0]);
+    }
+    return shuffle;
+}
