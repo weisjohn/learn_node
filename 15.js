0fe3e08a6f4a0b5a07f8f88fa29b3512fe3767ab
John Weis <weis.john@gmail.com>
2013-02-04 21:15:34 -0500

    adding commander example

---
+++ 15.js
@@ -0,0 +1,13 @@
+var program = require('commander'), 
+    randal = require('./14');
+
+program
+    .version('0.0.1')
+    .on('--help', function(){
+        console.log('  Example: \n');
+        console.log('    $ randal joe sally bob');
+        console.log('    bob joe sally\n');
+    })
+    .parse(process.argv);
+
+console.log(randal.apply(null, program.args).join(' '));
